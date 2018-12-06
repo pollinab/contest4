23dad0e87550b61f3b4b631491f58bba242cead1
pollinab <44142702+pollinab@users.noreply.github.com>
2018-12-06 19:36:23 +0300

    Update contest4_2.cpp

--- contest4_2.cpp
+++ contest4_2.cpp
@@ -1,3 +1,7 @@
+/*В большой IT-фирме есть только одна переговорная комната. 
+Желающие посовещаться заполняют заявки с желаемым временем начала и конца. Ваша задача определить максимальное количество заявок, 
+которое может быть удовлетворено. Число заявок ≤ 100000.*/
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -10,10 +14,10 @@ int main()
     {
         Requests.push_back(std::make_pair(finish, start));
     }
-    std::sort(Requests.begin(), Requests.end());
+    std::sort(Requests.begin(), Requests.end()); //сортируем заявки по времени конца
     int counter = 1;
     int currTime = Requests[0].first;
-    for (int i = 1; i < Requests.size(); ++i)
+    for (int i = 1; i < Requests.size(); ++i) //каждый раз выбираем первую по времени конца заявку, которая не пересекается с предыдущими
     {
         while (i < Requests.size() && Requests[i].second < currTime)
             ++i;
