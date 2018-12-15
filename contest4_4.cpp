762f9d8befbf73d92b7fd728dd6073bfa02c5195
pollinab <44142702+pollinab@users.noreply.github.com>
2018-12-15 15:39:41 +0300

    Update contest4_4.cpp

--- contest4_4.cpp
+++ contest4_4.cpp
@@ -1,13 +1,16 @@
+/*Дано невзвешенное дерево. Расстоянием между двумя вершинами будем называть количество ребер в пути, соединяющем эти две вершины. 
+Для каждой вершины определите расстояние до самой удаленной от нее вершины.*/
+
 #include<iostream>
 #include<vector>
 
-struct CNode
+struct CNode //вершина дерева
 {
-    int height;
-    int index_of_child1;
-    int index_of_child2;
-    int way_through_parent;
-    std::vector <CNode*> children;
+    int height; //длина максимального пути вниз до листа
+    int index_of_child1; //номер ребёнка, через которого проходит самый длинный путь вниз до листа
+    int index_of_child2; //номер ребёнка, через которого проходит второй по длине путь вниз до листа
+    int way_through_parent; //наибольшая длина пути через родителя
+    std::vector <CNode*> children; //дети
     CNode()
     {
         height = 0;
@@ -17,15 +20,15 @@ struct CNode
     }
 };
 
-void Up(std::vector <CNode*> tree)
+void Up(std::vector <CNode*> tree) //обход дерево снизу вверх с нахождением height для каждой вершины
 {
     int max1, max2;
     for (int j = tree.size() - 1; j >= 0; --j)
     {
         if (tree[j] -> children.size() != 0)
         {
-            max1 = -1;
-            max2 = -1;
+            max1 = -1; //максимум из height детей
+            max2 = -1; //второе по величине значение
             for (int i = 0; i < tree[j] -> children.size(); ++i)
             {
                 if (tree[j] -> children[i] -> height > max1)
@@ -45,8 +48,8 @@ void Up(std::vector <CNode*> tree)
         }
     }
 }
-
-void Down(CNode* root, int longest_way)
+//обход дерева сверху вниз с нахождением way_through_parent для каждой вершины
+void Down(CNode* root, int longest_way) //второй аргумент - наибольшая длина пути через родителя
 {
     root -> way_through_parent = longest_way;
     for (int i = 0; i < root -> children.size(); ++i)
@@ -60,7 +63,7 @@ void Down(CNode* root, int longest_way)
         {
             if (root -> index_of_child2 != -1)
                 longest_way = std::max(root -> children[root -> index_of_child2] -> height + 1, root -> way_through_parent);
-            else
+            else //если ребёнок один
                 longest_way = root -> way_through_parent;
             Down(root -> children[i], longest_way + 1);
         }
